688baf8fdca1de31586dfc8cb512e418297315b2
Ingo Reinhart <ingo.reinhart@gmail.com>
2015-06-21 16:54:48 +0200

    create Book Entity

---
+++ src/AppBundle/Entity/Book.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * @ORM\Entity
+ */
+class Book
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="Book", inversedBy="locations")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $location;
+
+    /**
+     * @ORM\Column(type="text")
+     * @Assert\NotBlank(message="Please don't leave title blank!")
+     * @Assert\Length(
+     *     min = "5",
+     *     minMessage = "Title is too short ({{ limit }} characters minimum)",
+     *     max = "50",
+     *     maxMessage = "Title is too long ({{ limit }} characters maximum)"
+     * )
+     */
+    private $title;
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    public function setTitle($title)
+    {
+        $this->title = $title;
+    }
+
+    public function getLocation()
+    {
+        return $this->location;
+    }
+
+    public function setLocation(Location $location = null)
+    {
+        $this->location = $location;
+    }
+}
